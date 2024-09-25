7a1a5a2deeb95febff869e97cf0de5043a18a9c4
jashan-lefty <jashanpreets2344@gmail.com>
2024-09-26 00:22:49 +1000

    Resolve

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-const {Builder, By, until} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    try {
-        await driver.get('http://www.google.com');
-        let searchBox = await driver.findElement(By.name('q'));
-        await searchBox.sendKeys('Selenium');
-        await driver.findElement(By.name('btnK')).click();
-        await driver.wait(until.titleIs('Selenium - Google Search'), 1000);
-    } finally {
-        await driver.quit();
-    }
+async function runTest() {
+  let options = new chrome.Options();
+  options.addArguments('--headless');  // Run Chrome in headless mode (no UI)
+  let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+
+  try {
+    await driver.get('http://www.google.com');
+    let searchBox = await driver.findElement(By.name('q'));
+    await searchBox.sendKeys('Selenium', Key.RETURN);
+    await driver.wait(until.titleContains('Selenium'), 1000);
+  } finally {
+    await driver.quit();
+  }
 }
 
-example();
+runTest();
